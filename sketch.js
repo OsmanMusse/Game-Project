2b4111e5179269710476bbbd5cee226d965aa153
Mezut <you@example.com>
2018-10-15 01:19:50 +0100

    adding the mountain, canon and trees

--- sketch.js
+++ sketch.js
@@ -48,7 +48,7 @@ function draw() {
 
 	noStroke();
 	fill(255);
-	text("mountain", 500, 256);
+	text("mountain", 670, 70);
 
 	//3. a tree
 	//... add your code here
@@ -64,7 +64,7 @@ function draw() {
 
 	noStroke();
 	fill(255);
-	text("canyon", 100, 480);
+	text("canyon", 180, 460);
 
 	//5. a collectable token - eg. a jewel, fruit, coins
 	//... add your code here
@@ -75,23 +75,70 @@ function draw() {
 
 
 	// Cloud Shape being modeled
-  beginShape();
-		fill(250);
-	ellipse(250, 150, 140, 90);
-	ellipse(160, 175, 100, 100);
-	ellipse(340, 145, 120, 110);
-	ellipse(370, 210, 105, 90);
-	ellipse(250, 220, 200, 100);
-
+    beginShape();
+     fill(250);
+     ellipse(250, 150, 140, 90);
+     ellipse(160, 175, 100, 100);
+     ellipse(340, 145, 120, 110);
+     ellipse(370, 210, 105, 90);
+     ellipse(250, 220, 200, 100);
 	endShape();
+    
+    fill(139,69,19);
+    rect(780, 450, 100, 155);
+    beginShape();
+    translate(769, 350);
+    fill(0,128,0);
+    triangle(10, 100, 58, 10, 112, 100);
+    endShape();
+    
+    // Cloud Shape ends here
+    
+    
+    // Mountain Shape Starts here
 
-  triangle(30, 75, 58, 20, 86, 74);
-	fill(165,42,42, 42);
-	rect(780, 450, 105, 155);
+    beginShape();
+    fill(66,65,73);
+    translate(100, -230);
+    triangle(0, -120, 158, 185, -100, 185);
+    
+
+    fill(62,69,73);
+    translate(-150, 10);
+    triangle(20, -100, 158, 175, -100, 175);
+    
+    fill(64,65,73);
+    translate(-180, 0);
+    triangle(80, -100, 158, 175, -100, 175);
+    
+    endShape();
+    
+
+// Mountain Shape End Here
+    
+    
+    
+// Canon Shape Start Here
+    beginShape();
+    
+    fill(0,0,0);
+    rect(-400, 345, 120, 50);
+    
+    
+    fill(50,0,0);
+    ellipse(-380, 390, 55, 55);
+    
+    
+    endShape();
+// Canon Shape Ends Here
 
 }
 
-// Cloud Shape ends here
+
+
+
+
+
 
 
 // Tree Shape being modeled
